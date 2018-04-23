6d3cd942bae450d4ba16f3c4d153db1ac13e800e
Trevor Seitz <trevinla@gmail.com>
2018-04-23 15:43:42 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,7 +14,8 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    binding.pry
-    self.artists.name
+    self.artists.each do |artist|
+      list << artist.name
+    end
   end
 end
